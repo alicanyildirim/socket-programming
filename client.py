281200b249c49816fb19082f491581bdab7bf651
Alican Yildirim <alicanyildirim96@outlook.com>
2020-12-12 16:22:10 +0200

    added comments

--- client.py
+++ client.py
@@ -1,2 +1,7 @@
 # Client takes -> server ip, server's udp listen port, server's tcp listen port,
 # client's sender port for udp, client's sender port for tcp through cl args.
+
+# only client can read the files.
+# client should divide the both files into chunks of up to 1000bytes before sending. -> fragmentation.
+# transfer files.
+# timeout?
--- server.py
+++ server.py
@@ -1,2 +1,18 @@
 # cl args listen port for udp, listen port for tcp
 
+# realize when the whole file is transferred then stop listening, reassamble, save the files.
+
+# calculate the average packet transmisson times for udp and tcp
+# start the time before sending a packet in client. 
+# stop the time after packet is recieved in server.
+# If you resend a packet only consider the last packet.
+
+#Print average as..
+#TCP Packets Average Transmission Time: ... ms
+#UDP Packets Average Transmission Time: ... ms
+#TCP Communication Total Transmission Time: ... ms
+#UDP Communication Total Transmission Time: ... ms
+
+
+# in the client -> UDP Transmission Re-transferred Packets: ...
+
