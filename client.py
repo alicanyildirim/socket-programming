2dcb06b94162b3c923c16bd09f76a167d5810976
Alican Yildirim <alicanyildirim96@outlook.com>
2020-12-14 01:14:49 +0200

    tcp done

--- client.py
+++ client.py
@@ -21,76 +21,84 @@ server_tcp_listen = argv[2]
 client_udp_sender = argv[3]
 client_tcp_sender = argv[4]
 
-tcp_file = "transfer_file_TCP.txt"
+tcp_file_original = "transfer_file_TCP.txt"
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 
+def run_tcp_client(tcp_file,HOST,PORT):
+    # the file we will send via tcp
+    #print(sys.getsizeof(tcp_str))    
+    #print(len(tcp_str))    
+    tcp_file_raw  = open(tcp_file,'rb')
+    tcp_str = tcp_file_raw.read()
 
 
-# the file we will send via tcp
-#print(sys.getsizeof(tcp_str))    
-#print(len(tcp_str))    
-tcp_file_raw  = open(tcp_file,'r')
-tcp_str = tcp_file_raw.read()
+    # create streaming socket
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish tcp connection with the server
+
+    # needs to be tuple apparently
+    tcp_socket.connect((HOST,PORT))
+
+    #print(sys.getsizeof(sliced))    
+    # split data into chunks.
+    #print(len(chunks))    
+    # split data into chunks.
+    chunks = [tcp_str[i:i+1000] for i in range(0, len(tcp_str), 1000)]
+    for chunk in chunks:
+        #print(f"Sending {i}")
+        #print("a")
+
+        # gives 1033 bytes 
+        #print(sys.getsizeof(chunk.encode('utf-8')))    
+
+        # need to make it byte 
+        byte_chunk = bytearray(chunk)
+
+        # using 'd' -> double will store 8 bytes.
+        # send the time with the data to the server.
+        message = struct.pack("d", time.time()) + byte_chunk
+        tcp_socket.send(message)
+        time.sleep(0.1)
 
 
-# create streaming socket
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # need to encode, md5 throws error otherwise
 
-# establish tcp connection with the server
+    # need to encode, md5 throws error otherwise
+    #tcp_check = tcp_str.encode('utf-8')
 
-# needs to be tuple apparently
-tcp_socket.connect((HOST,PORT))
-
-#print(sys.getsizeof(sliced))    
-# split data into chunks.
-#print(len(chunks))    
-# split data into chunks.
-chunks = [tcp_str[i:i+1000] for i in range(0, len(tcp_str), 1000)]
-for chunk in chunks:
-    #print(f"Sending {i}")
-    #print("a")
-
-    # gives 1033 bytes 
-    #print(sys.getsizeof(chunk.encode('utf-8')))    
-
-    # need to make it byte 
-    byte_chunk = chunk.encode('utf-8')
-
-    # using 'd' -> double will store 8 bytes.
-    # send the time with the data to the server.
-    message = struct.pack("d", time.time()) + byte_chunk
-    tcp_socket.send(message)
-    time.sleep(0.1)
-
-
-# need to encode, md5 throws error otherwise
-
-# need to encode, md5 throws error otherwise
-#tcp_check = tcp_str.encode('utf-8')
-
-tcp_file_raw  = open(tcp_file,'rb')
-tcp_str = tcp_file_raw.read()
-print(hashlib.md5(tcp_str).hexdigest())
+    tcp_file_raw.close()
+    tcp_file_raw  = open(tcp_file,'rb')
+    tcp_str = tcp_file_raw.read()
+    print(hashlib.md5(tcp_str).hexdigest())
 
 
 
 
-# this also prints a newline
-#print(tcp_file_raw.read())
+    # this also prints a newline
+    #print(tcp_file_raw.read())
 
-# tcp_file_raw.close()
+    # tcp_file_raw.close()
 
-'''
-# the file we will send via tcp
-tcp_file_raw = open(tcp_file,'r')
-tcp_str = tcp_file_raw.read()
+    '''
+    # the file we will send via tcp
+    tcp_file_raw = open(tcp_file,'r')
+    tcp_str = tcp_file_raw.read()
 
-# need to encode, md5 throws error otherwise
-a = tcp_str[:1000].encode('utf-8')
+    # need to encode, md5 throws error otherwise
+    a = tcp_str[:1000].encode('utf-8')
 
 
-print(hashlib.md5(a).hexdigest())
-'''
+    print(hashlib.md5(a).hexdigest())
+    '''
+
+#run_tcp_client(tcp_file_original,HOST,PORT)
+#run_tcp_client("test1.bin",HOST,PORT)
+#run_tcp_client("lorem.txt",HOST,PORT)
+#run_tcp_client("test1.bin",HOST,PORT)
+#run_tcp_client("test2.bin",HOST,PORT)
+#run_tcp_client("test3.bin",HOST,PORT)
+run_tcp_client("test4.bin",HOST,PORT)
--- server.py
+++ server.py
@@ -24,51 +24,68 @@ import time
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-
-# TCP connection
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# the server only gets the ports, how to know the host?
-tcp_socket.bind((HOST, PORT))
-
-# listen 
-tcp_socket.listen()
-
-# accept the connection
-client_socket, client_addr = tcp_socket.accept()
-
-reassamble = []
-while True:
-    # message is 1033 bytes + 8 bytes = 1041 bytes
-    message = client_socket.recv(1041)
-    if message == b'':
-        client_socket.close()
-        break 
-
-
-    t = struct.unpack("d", message[:8])[0]
-    passing_time = time.time() - t
-    # get the time information
-    #print(passing_time)
-    # get the payload.
-    payload = message[8:]
-    reassamble.append(payload)
-
-length = len(reassamble)
-with open("tcp_out.txt", "wb") as output_file:
-	for i in range(length):
-		output_file.write(reassamble[i])
-# now the reassamble is a str
+def run_tcp_server(output,HOST,PORT):
+    # TCP connection
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # the server only gets the ports, how to know the host?
+    tcp_socket.bind((HOST, PORT))
+    # listen 
+    tcp_socket.listen()
+    # accept the connection
+    client_socket, _ = tcp_socket.accept()
+    reassamble = []
+    elapsed_times = []
+    start_times = []
+    while True:
+        # message is 1033 bytes + 8 bytes = 1041 bytes
+        message = client_socket.recv(1041)
+        if message == b'':
+            #TCP Packets Average Transmission Time: ... ms
+            average_tcp_time = sum(elapsed_times) / len(elapsed_times) 
+            print(f"TCP Packets Average Transmission Time: {average_tcp_time:.4} ms" )
+            
+            #TCP Communication Total Transmission Time: ... ms
+            total_time = (time.time()-start_times[0])*100
+            print(f"TCP Communication Total Transmission Time: {total_time:.4} ms" )
+            client_socket.close()
+            break 
 
 
-tcp_file_raw  = open("tcp_out.txt",'rb')
-tcp_str = tcp_file_raw.read()
-tcp_file_raw.close()
-# need to encode, md5 throws error otherwise
-#tcp_check = tcp_str.encode('latin-1')
-print(hashlib.md5(tcp_str).hexdigest())
+        # get the time information
+        t = struct.unpack("d", message[:8])[0]
+        passing_time = time.time() - t
+        start_times.append(t)
+        #from sec to msec
+        elapsed_times.append(passing_time*100)
+        
+        # get the payload.
+        payload = message[8:]
+        reassamble.append(payload)
+    length = len(reassamble)
+    with open(output, "wb") as output_file:
+        for i in range(length):
+            output_file.write(reassamble[i])
+    
+    
+    # need to encode, md5 throws error otherwise
+    tcp_file_raw  = open(output,'rb')
+    tcp_str = tcp_file_raw.read()
+    print(hashlib.md5(tcp_str).hexdigest())
+    tcp_file_raw.close()
+    #tcp_check = tcp_str.encode('latin-1')
 
-# split the payload into chunks.
+    # split the payload into chunks.
+output = "tcp_out1.txt"
+#run_tcp_server(output,HOST,PORT)
+
+#run_tcp_server("out_lorem.txt",HOST,PORT)
+#run_tcp_server("out1.bin",HOST,PORT)
+#run_tcp_server("out2.bin",HOST,PORT)
+#run_tcp_server("out3.bin",HOST,PORT)
+run_tcp_server("out4.bin",HOST,PORT)
+
+
+
 
 # checksum using pseudo header + tcp header + tcp body
 
